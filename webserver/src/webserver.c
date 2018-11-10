2482fc011218ec4396ea3726e8cf34828bc4dba6
Wahaj Javed <major.payne141@gmail.com>
2018-11-10 13:29:20 +0500

    Added basic project structure

---
+++ webserver/src/webserver.c
@@ -0,0 +1,19 @@
+/*
+ * webserver.c
+ *
+ *  Created on: Nov 10, 2018
+ *      Author: wj
+ */
+
+
+#include <stdio.h>
+
+#define PORT "10000"
+#define MAX_CONNECTIONS 1024
+#define SERVER_ROOT "."
+
+
+
+int main() {
+	printf("Helo");
+}
